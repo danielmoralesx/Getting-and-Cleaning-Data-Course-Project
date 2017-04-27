f5759eccce95a34ff2e8064c7d6fcaa240c77306
Daniel Morales <danielmoralesx@hotmail.com>
2017-04-27 15:53:24 -0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -22,8 +22,8 @@ subject <- rbind(read.table("UCI HAR Dataset//test//subject_test.txt"),
 joint_data <- cbind(subject, activity, joint_data)
 names(joint_data) <- c("subject", "activity", features[indexes])
 
-# From the data set in step 4, creates a second, independent tidy data set with 
-# the average of each variable for each activity and each subject.
+# 5.From the data set in step 4, creates a second, independent tidy data set
+# with the average of each variable for each activity and each subject.
 tidy_data_set <- joint_data %>% 
   group_by(subject, activity) %>% 
   summarise_each(funs(mean))
